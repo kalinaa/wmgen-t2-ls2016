3cb566634141fdc1eebf19862dc9a5d7915390d4
Alexandr Kalinin <alexkalinin89@yandex.ru>
2016-03-02 20:53:38 +0300

    add slider, add style for flider in _setings.scss

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,5 @@
 /*
  * Custom
  */
-
-;(function(){
-
-  $('.position__first').on('click', function(){
-    $('.position__watermark-one').removeClass('hide');
-    $('.position__watermark-second').addClass('hide');
-  });
-
-  $('.position__second').on('click', function(){
-    $('.position__watermark-second').removeClass('hide');
-    $('.position__watermark-one').addClass('hide');
-  });
-
-}());
+//= partials/styles.js
+//= partials/opacity-slider.js
