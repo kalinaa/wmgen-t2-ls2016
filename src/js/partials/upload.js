17e52f81a4e7e32af11ac9fe0e156798e643d322
Alexandr Kalinin <alexkalinin89@yandex.ru>
2016-03-03 15:25:13 +0300

    upload module add push file name in placeholder

---
+++ src/js/partials/upload.js
@@ -0,0 +1,43 @@
+;$(function () {
+    'use strict';
+    var mainImg     = $('#main-img-input'),
+        watermark   = $('#watermark-input'),
+        url         = '../server/php/';
+    mainImg.fileupload({
+        url: url,
+        dataType: 'json',
+        done: function (e, data) {
+            var fileType = data.result.files[0].type,
+                fileExt = fileType.substring(fileType.lastIndexOf('/')+1, fileType.length);
+            $('#main-img-input').siblings('.file-name').text(data.result.files[0].name);
+            console.log(data.result.files[0].name);
+            console.log(data.result.files[0].url);
+            console.log(data.result.files[0].type);
+            console.log(fileExt);
+        },
+        progress: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            if (progress != 100) {
+                console.log(progress);
+            }
+        }
+    });
+    watermark.fileupload({
+        url: url,
+        dataType: 'json',
+        done: function (e, data) {
+            var fileType = data.result.files[0].type,
+                fileExt = fileType.substring(fileType.lastIndexOf('/')+1, fileType.length);
+            $('#watermark-input').siblings('.file-name').text(data.result.files[0].name);
+            console.log(data.result.files[0].url);
+            console.log(data.result.files[0].type);
+            console.log(fileExt);
+        },
+        progress: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            if (progress != 100) {
+                console.log(progress);
+            }
+        }
+    })
+});
