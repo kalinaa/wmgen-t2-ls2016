ec3263991ddf12f4997f0b155c108189e56e9683
platonovAlexey <kalyawe4ka@gmail.com>
2016-03-05 16:02:36 +0300

    edit reset, add drag&drop

--- src/js/partials/reset.js
+++ src/js/partials/reset.js
@@ -2,5 +2,6 @@
 	$('.settings__form').find('input').val('');
 	$('.file-name').html('');
 	$('#main-img, #watermark').removeAttr("alt").removeAttr("src");
-	$('.ui-slider-handle').css('left','50%');
+	$('.ui-slider-handle').css('left','100%');
+	$('#trans-progress').css('width','100%');
 });
