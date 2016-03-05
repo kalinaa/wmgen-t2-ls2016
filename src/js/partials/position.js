2ed306c3459893079a4ea4b9b3718d3890898f74
Alexander Kravchenko <alex.krava@live.com>
2016-03-05 16:02:47 +0200

    enter inputs

--- src/js/partials/position.js
+++ src/js/partials/position.js
@@ -9,25 +9,35 @@ var position = (function () {
                 e.preventDefault();
                 var pos = $(this).data("pos");
 
-                $(".img_small").position({
+                $('.img_small').position({
                     my: pos,  // место на позиционируемом элементе
                     at: pos,  // место на элементе относительно которого будет позиционирование
                     collision: 'none none',
-                    of: ".img_big" // элемент относительно которого будет позиционирование
+                    of: '.img_big' // элемент относительно которого будет позиционирование
                 });
 
                 //поиск и вывод координатов водяного знака
                 
-                var top = Math.round($(".img_small").offset().top).toFixed(0) - Math.round($(".img_big").offset().top).toFixed(0);
-                var left = Math.round($(".img_small").offset().left).toFixed(0) - Math.round($(".img_big").offset().left).toFixed(0);
+                var top = Math.round($('.img_small').offset().top).toFixed(0) - Math.round($('.img_big').offset().top).toFixed(0);
+                var left = Math.round($('.img_small').offset().left).toFixed(0) - Math.round($('.img_big').offset().left).toFixed(0);
 
                 $('.input_x').val(left);
                 $('.input_y').val(top);
-
-                console.log(top);
-                console.log(left);
             });
-
+            
+            //задает положение через ввод данных в input по оси Х
+            inputXY ($('.input_x'), $('.img_small'), 'left');
+            
+            //задает положение через ввод данных в input по оси Y
+            inputXY ($('.input_y'), $('.img_small'), 'top');
+            
+            function inputXY (input, img, position) {
+                input.on('keyup', function () {
+                    var valueX = input.val();
+                    img.css(position, valueX + 'px' );
+                });
+            }
+            
         };
 
         return {
