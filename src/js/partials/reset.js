27706427146cd29ace08d69a345b2ef1bb69d763
Alexandr Kalinin <alexkalinin89@yandex.ru>
2016-03-05 18:47:43 +0300

    add disabled function, modified reset function

--- src/js/partials/reset.js
+++ src/js/partials/reset.js
@@ -1,7 +1,13 @@
-;$('.button-reset').on('click', function(){
-	$('.settings__form').find('input').val('');
-	$('.file-name').html('');
-	$('#main-img, #watermark').removeAttr("alt").removeAttr("src");
-	$('.ui-slider-handle').css('left','100%');
-	$('#trans-progress').css('width','100%');
+;$(document).ready(function(){
+	var disabledNode = $('.disabled');
+	$('.button-reset').on('click', function(){
+		$('.settings__form').find('input').val('');
+		$('.file-name').html('');
+		$('#main-img, #watermark').removeAttr("alt").removeAttr("src");
+		$('.ui-slider-handle').css('left','100%');
+		$('#trans-progress').css('width','100%');
+		disabledNode.each(function(){
+			$(this).addClass('disabled');
+		});
+	});
 });
--- src/js/partials/upload.js
+++ src/js/partials/upload.js
@@ -1,38 +1,35 @@
 ;$(function () {
     'use strict';
-    var mainImg     = $('#main-img-input'),
-        watermark   = $('#watermark-input'),
-        url         = '../server/php/';
+    var mainImg      = $('#main-img-input'),
+        watermark    = $('#watermark-input'),
+        url          = '../server/php/',
+        disabledNode = $('.disabled');
     mainImg.fileupload({
-        //add: function(e, data) {
-        //    var uploadErrors = [];
-        //    var acceptFileTypes = /^image\/(gif|jpe?g|png)$/i;
-        //    if(data.originalFiles[0].type.length > 0 && !acceptFileTypes.test(data.originalFiles[0].type)) {
-        //        uploadErrors.push('Not an accepted file type');
-        //    }
-        //    if(data.originalFiles[0].size > 1000000) {
-        //        uploadErrors.push('Filesize is too big');
-        //    }
-        //    if(uploadErrors.length > 0) {
-        //        alert(uploadErrors.join("\n"));
-        //    } else {
-        //        data.submit();
-        //        console.log('Работает!')
-        //    }
-        //},
+        add: function(e, data) {
+            var uploadErrors = [];
+            var acceptFileTypes = /^image\/(gif|jpe?g|png)$/i;
+            if(!acceptFileTypes.test(data.originalFiles[0].type)) {
+                uploadErrors.push('Загрузите картинку');
+            }
+            if(data.originalFiles[0].size > 2000000) {
+                uploadErrors.push('Файл слишком большой');
+            }
+            if(uploadErrors.length > 0) {
+                alert(uploadErrors.join("\n"));
+            } else {
+                data.submit();
+                console.log('Файл загружен')
+            }
+        },
         url: url,
         dataType: 'json',
         done: function (e, data) {
-            var fileType = data.result.files[0].type,
-                fileExt = fileType.substring(fileType.lastIndexOf('/')+1, fileType.length);
             $('#main-img-input').siblings('.file-name').text(data.result.files[0].name);   // подстановка имени файла в инпуты
             $('#main-img').attr('src', data.result.files[0].url);   // передача адреса картинки в канву
             if ($('#watermark-input').prop('disabled')){
                 $('#watermark-input').prop('disabled', false);     // разблокировка второго input
-                $('#watermark-input').parent().removeClass('disabled');
-            };
-            console.log(data.result.files[0].type);
-            console.log(fileExt);
+                $('#watermark-wrap').children('.disabled').removeClass('disabled');
+            }
         },
         //включение анимации прогресса при загрузку тяжелых файлов
         progress: function (e, data) {
@@ -46,12 +43,11 @@
         url: url,
         dataType: 'json',
         done: function (e, data) {
-            var fileType = data.result.files[0].type,
-                fileExt = fileType.substring(fileType.lastIndexOf('/')+1, fileType.length);
             $('#watermark-input').siblings('.file-name').text(data.result.files[0].name);
             $('#watermark').attr('src', data.result.files[0].url);
-            console.log(data.result.files[0].type);
-            console.log(fileExt);
+            disabledNode.each(function(){
+                $(this).removeClass('disabled');
+            });
         },
         //включение анимации прогресса при загрузку тяжелых файлов
         progress: function (e, data) {
