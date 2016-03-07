3d8f0a266f3a2196dc8ded21772321238046449d
Vik <kvvendetta@gmail.com>
2016-03-07 14:22:08 +0200

    Add socials.

--- src/js/partials/styles.js
+++ src/js/partials/styles.js
@@ -24,12 +24,6 @@
             url += '&noparse=true';
             Share.popup(url);
         },
-        odnoklassniki: function(purl, text) {
-            url  = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
-            url += '&st.comments=' + encodeURIComponent(text);
-            url += '&st._surl='    + encodeURIComponent(purl);
-            Share.popup(url);
-        },
         facebook: function(purl, ptitle, pimg, text) {
             url  = 'http://www.facebook.com/sharer.php?s=100';
             url += '&p[title]='     + encodeURIComponent(ptitle);
@@ -44,14 +38,6 @@
             url += '&url='      + encodeURIComponent(purl);
             url += '&counturl=' + encodeURIComponent(purl);
             Share.popup(url);
-        },
-        mailru: function(purl, ptitle, pimg, text) {
-            url  = 'http://connect.mail.ru/share?';
-            url += 'url='          + encodeURIComponent(purl);
-            url += '&title='       + encodeURIComponent(ptitle);
-            url += '&description=' + encodeURIComponent(text);
-            url += '&imageurl='    + encodeURIComponent(pimg);
-            Share.popup(url)
         },
 
         popup: function(url) {
