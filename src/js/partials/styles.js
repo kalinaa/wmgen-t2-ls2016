4c3d0da1c0596d75d6f3fac07d81d5b5d70e2bd2
Alexander Kravchenko <alex.krava@live.com>
2016-03-09 10:55:00 +0200

    toggle position

--- src/js/partials/styles.js
+++ src/js/partials/styles.js
@@ -2,16 +2,20 @@
 
     $('.position__first').on('click', function(e){
         e.preventDefault();
-
-        $('.position__watermark-one').removeClass('hide');
-        $('.position__watermark-second').addClass('hide');
+        
+        $('.position__watermark-second').removeClass('hide');
+        $('.position__watermark-one').addClass('hide');
+        $(this).addClass('position__first--active');
+        $('.position__second').removeClass('position__second--active');
     });
 
     $('.position__second').on('click', function(e){
         e.preventDefault();
 
-        $('.position__watermark-second').removeClass('hide');
-        $('.position__watermark-one').addClass('hide');
+        $('.position__watermark-one').removeClass('hide');
+        $('.position__watermark-second').addClass('hide');
+        $(this).addClass('position__second--active');
+        $('.position__first').removeClass('position__first--active');
     });
 
 
