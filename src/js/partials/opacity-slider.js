c4440e9239b00b2d87111569bc79c66d35eddab3
Alexandr Kalinin <alexkalinin89@yandex.ru>
2016-03-11 21:52:01 +0300

    modified opacity , upload modules for tilling

--- src/js/partials/opacity-slider.js
+++ src/js/partials/opacity-slider.js
@@ -1,13 +1,13 @@
 ;(function (){
     var opacitySlider = $('#slider'),
-        smallImg =      $('#watermark'),
+        watermark =      $('#wm-wrap'),
         progress =      $('#trans-progress');
         watermarkOpacity =       1;
         opacitySlider.slider({
             value: 100,
             slide: function(event, value){
                 watermarkOpacity = value.value/100;
-                smallImg.fadeTo(0, watermarkOpacity);
+                watermark.fadeTo(0, watermarkOpacity);
                 progress.width(value.value + '%');
             },
             change: function() {
