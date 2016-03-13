3c004b2484e8837d028c591541e3d4e3b08fd3ba
George Astakhov <george75@list.ru>
2016-03-13 20:04:12 +0300

    Add getimage.php

--- src/server/php/getsize.php
+++ src/server/php/getsize.php
@@ -3,7 +3,7 @@
 	$image_width = 0;
 	$image_height = 0;
 
-	if (isset($_POST['$url'])) {
+	if (isset($_POST['url'])) {
 		$url = $_POST['url'];
 		$image_get = true;
 		list($image_width, $image_height) = getimagesize($url);
