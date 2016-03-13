5d094638b6db4e70a2ca6fc77670762e388a4837
Alexander Kravchenko <alex.krava@live.com>
2016-03-13 23:52:26 +0200

    fix bug reset.js

--- src/js/partials/reset.js
+++ src/js/partials/reset.js
@@ -3,11 +3,10 @@
 	$('.button-reset').on('click', function(){
 		$('.settings__form').find('input').val('');
 		$('.file-name').text('');
-		imgSettings.tiling = false;
-		$('.position__second').trigger('click');
 		$('#main-img, #watermark').removeAttr("alt").removeAttr("src");
 		$('.ui-slider-handle').css('left','100%');
 		$('#trans-progress').css('width','100%');
+        $('.watermark-first').removeClass('watermark-link--active');        
 		disabledNode.each(function(){
 			$(this).addClass('disabled');
 			$('#watermark-input').prop('disabled', true);
